f752f0b3ea818a8eb2f89ea3714666746c37ffab
William Tsay <willtsay@gmail.com>
2014-09-14 15:00:56 -0700

    stuff

--- realtime.js
+++ realtime.js
@@ -1,19 +1,6 @@
 var io = require('socket.io')(process.env.PORT||8080)
 
-var texts = []
-
-
 io.sockets.on('connection', function(socket){
-  socket.on('echo', function(data){
-    socket.emit('echo',data)
-  })
-  socket.on('echo-ack', function(data, callback){
-    callback(data)
-  })
-  socket.on('send:text', function(data){
-    texts.push(data)
-    io.sockets.emit('send:text', texts)
-  })
   socket.on('change:color', function(data){
     io.sockets.emit('change:color', data)
   })
